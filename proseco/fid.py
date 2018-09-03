0c54da36eed43c0dfcd8d34848d54104d8332236
Tom Aldcroft <taldcroft@gmail.com>
2018-09-03 17:02:10 -0400

    Initial fid catalog selection and tests

--- proseco/fid.py
+++ proseco/fid.py
@@ -35,9 +35,14 @@ def get_fid_catalog(*, detector=None, focus_offset=0, sim_offset=0,
 
     fids.meta['cand_fids'] = fids.get_fid_candidates()
 
-    # Get an initial fid catalog based on the rank of field stars spoiling fids
-    # and fids spoiling current acq star within current acq search box.
-    fids.get_initial_catalog()
+    # Set initial fid catalog if possible to a set for which no field stars
+    # spoiler any of the fid lights and no fid lights is a search spoilers for
+    # any current acq star.  If not possible then the table is still zero
+    # length and we need to fall through to the optimization process.
+    fids.set_initial_catalog()
+
+    # Add a `slot` column that makes sense
+    fids.set_slot_column()
 
     return fids
 
@@ -76,7 +81,7 @@ class FidTable(ACACatalogTable):
                 dither = acqs.meta['dither']
             if print_log is None:
                 print_log = acqs.print_log
-            acqs = weakref.ref(acqs)
+            self.acqs = acqs
 
         super().__init__(data=data, print_log=print_log, **kwargs)
 
@@ -87,25 +92,112 @@ class FidTable(ACACatalogTable):
                           'dither': dither,
                           'stars': stars})
 
-    def get_initial_catalog(self):
+    @property
+    def acqs(self):
+        return self._acqs() if hasattr(self, '_acqs') else None
+
+    @acqs.setter
+    def acqs(self, val):
+        self._acqs = weakref.ref(val)
+
+    def set_slot_column(self):
+        """
+        Set the `slot` column.
+        """
+        if len(self) > 0:
+            # Sort to make order match the original candidate list order (by
+            # increasing mag), and assign a slot.
+            self['slot'] = np.arange(len(self))
+
+            # Add slot to cand_fids table, putting in '...' if not selected as acq.
+            # This is for convenience in downstream reporting or introspection.
+            cand_fids = self.meta['cand_fids']
+            slots = [str(self.get_id(fid['id'])['slot']) if fid['id'] in self['id'] else '...'
+                     for fid in cand_fids]
+            cand_fids['slot'] = slots
+
+
+    def set_initial_catalog(self):
+        """Set initial fid catalog (fid set) if possible to the first set which is
+        "perfect":
+
+        - No field stars spoiler any of the fid lights
+        - Fid lights are not search spoilers for any of the current acq stars
+
+        If not possible then the table is still zero length and we will need to
+        fall through to the optimization process.
+
+        """
+        # Start by getting the id of every fid that has a zero spoiler score,
+        # meaning no star spoils the fid as set previously in get_initial_candidates.
         cand_fids = self.meta['cand_fids']
-        cand_fids_set = set(cand_fids['id'])
+        cand_fids_set = set(fid['id'] for fid in cand_fids if fid['spoiler_score'] == 0)
 
-        for fid_set in self.meta['fid_sets']:
-            if fid_set <= cand_fids_set:
-                # If no acqs then just take the first allowed fid set and jump out
-                if self.acqs is None:
-                    cand_fids_ids = cand_fids['id'].tolist()
-                    idxs = [cand_fids.iloc(fid_id) for fid_id in sorted(fid_set)]
-                    # Transfer to self (which at this point is an empty table)
-                    for name, col in cand_fids.columns.items():
-                        self[name] = col[idxs]
-                    return
+        # Get list of fid_sets that are consistent with candidate fids. These
+        # fid sets are the combinations of 3 (or 2) fid lights in preferred
+        # order.
+        fid_sets = FID.fid_sets[self.meta['detector']]
+        ok_fid_sets = [fid_set for fid_set in fid_sets if fid_set <= cand_fids_set]
 
+        # If no fid_sets are possible, leave self as a zero-length table
+        if not ok_fid_sets:
+            self.log('No acceptable fid sets (off-CCD or spoiled by field stars)')
+            return
+
+        # If no acq stars then just pick the first allowed fid set.
+        if self.acqs is None:
+            fid_set = ok_fid_sets[0]
+            self.log(f'No acq stars available, using first OK fid set {fid_set}')
+
+        else:
+            spoils_any_acq = {}
+
+            for fid_set in ok_fid_sets:
+                self.log(f'Checking fid set {fid_set} for acq star spoilers', level=1)
                 for fid_id in fid_set:
-                    fid = cand_fids.get_id(fid_id)
+                    if fid_id not in spoils_any_acq:
+                        fid = cand_fids.get_id(fid_id)
+                        spoils_any_acq[fid_id] = any(self.spoils(fid, acq)
+                                                     for acq in self.acqs)
+                    if spoils_any_acq[fid_id]:
+                        # Loser, don't bother with the rest.
+                        self.log(f'Fid {fid_id} spoils an acq star', level=2)
+                        break
+                else:
+                    # We have a winner, none of the fid_ids in current fid set
+                    # will spoil any acquisition star.  Break out of loop with
+                    # fid_set as the winner.
+                    self.log(f'Fid set {fid_set} is fine for acq stars')
+                    break
+            else:
+                # Tried every set and none were acceptable.
+                fid_set = None
+                self.log('No acceptable fid set found')
+
+        if fid_set is not None:
+            # Transfer fid set columns to self (which at this point is an empty
+            # table)
+            idxs = [cand_fids.get_id_idx(fid_id) for fid_id in sorted(fid_set)]
+            for name, col in cand_fids.columns.items():
+                self[name] = col[idxs]
 
 
+    def spoils(self, fid, acq):
+        """
+        Return true if ``fid`` could be within ``acq`` search box.
+
+        Includes:
+        - 20" (4 pix) positional err on fid light
+        - 4 pixel readout halfw for fid light
+        - 2 pixel PSF of fid light that could creep into search box
+        - Acq search box half-width
+        - Dither amplitude (since OBC adjusts search box for dither)
+        """
+        spoiler_margin = (FID.spoiler_margin +
+                          self.acqs.meta['dither'] / 5 +
+                          acq['halfw'] / 5)
+        return (np.abs(fid['row'] - acq['row']) < spoiler_margin and
+                np.abs(fid['col'] - acq['col']) < spoiler_margin)
 
 
     def get_fid_candidates(self):
@@ -123,12 +215,11 @@ class FidTable(ACACatalogTable):
         row, col = yagzag_to_pixels(yang, zang, allow_bad=True)
         ids = np.arange(len(yang), dtype=np.int64) + 1  # E.g. 1 to 6 for ACIS
 
-        # Set up candidate fids table and add columns
+        # Set up candidate fids table (which copies relevant meta data) and add
+        # columns.
         cand_fids = FidTable([ids, yang, zang, row, col],
                              names=['id', 'yang', 'zang', 'row', 'col'])
-
-        # Copy meta from parent FidTable
-        # cand_fids.meta.update(self.meta)
+        self.log(f'Initial candidate fid ids are {cand_fids["id"].tolist()}')
 
         # Reject (entirely) candidates that are off CCD or have a bad pixel
         self.reject_off_ccd(cand_fids)
@@ -138,68 +229,71 @@ class FidTable(ACACatalogTable):
         cand_fids['spoilers'] = None  # Filled in with Table of spoilers
         cand_fids['spoiler_score'] = np.int64(0)
 
-        # Make sure cand_fids meta doesn't have its own cand_fids (in case of
-        # non-standard call order)
-        if 'cand_fids' in self.meta:
-            del self.meta['cand_fids']
-
-        # If acqs table available that means stars and dark are available to
-        # find stars or bad pixels (imposters) that are bad for fid.
-        if self.meta['acqs']:
+        # If stars are available then find stars that are bad for fid.
+        if self.meta['stars']:
             for fid in cand_fids:
-                fid['spoilers'] = self.get_spoiler_stars(fid)
+                self.set_spoilers_score(fid)
 
         return cand_fids
 
     def reject_off_ccd(self, cand_fids):
-        """Filter out candidates that are outside allowed CCD region"""
+        """Filter out candidates that are outside allowed CCD region.
+
+        This updates ``cand_fids`` in place.
+
+        :param cand_fids: table of candidate fid lights (on CCD)
+        """
         n_cand_fids = len(cand_fids)
-        idx_bad = []
+        idx_bads = []
+
         for idx, fid in enumerate(cand_fids):
             if ((np.abs(fid['row']) + FID.ccd_edge_margin > ACQ.max_ccd_row) or
                     (np.abs(fid['col']) + FID.ccd_edge_margin > ACQ.max_ccd_col)):
                 # If this would result in fewer than 2 fid lights in catalog then
                 # just stop rejecting.  It will be picked up in review.
-                if n_cand_fids - len(idx_bad) <= 2:
-                    self.log('ERROR: fewer than 2 good fids found, accepting off-CCD fid light(s)')
+                if n_cand_fids - len(idx_bads) <= 2:
+                    self.log('ERROR: fewer than 2 good fids found, accepting '
+                             'off-CCD fid light(s)', level=1)
                     break
 
                 self.log(f'Rejecting fid id={fid["id"]} row,col='
-                         f'({fid["row"]:.1f}, {fid["col"]:.1f}) off CCD')
-                idx_bad.append(idx)
+                         f'({fid["row"]:.1f}, {fid["col"]:.1f}) off CCD',
+                         level=1)
+                idx_bads.append(idx)
 
-        if idx_bad:
-            cand_fids.remove_rows(idx_bad)
+        if idx_bads:
+            cand_fids.remove_rows(idx_bads)
 
     def reject_bad_pixel(self, cand_fids):
         """Filter out candidates that have a bad pixel too close"""
         n_cand_fids = len(cand_fids)
-        idx_bad = []
+        idx_bads = []
         for idx, fid in enumerate(cand_fids):
             # TO BE IMPLEMENTED
             if False:
                 # If this would result in fewer than 2 fid lights in catalog then
                 # just stop.  It will be picked up in review.
-                if n_cand_fids - len(idx_bad) <= 2:
+                if n_cand_fids - len(idx_bads) <= 2:
                     self.log('ERROR: fewer than 2 good fids found, accepting bad fid light(s)')
                     break
 
                 self.log(f'Rejecting fid {fid["id"]}: near bad pixel')
-                idx_bad.append(idx)
+                idx_bads.append(idx)
 
-        if idx_bad:
-            cand_fids.remove_rows(idx_bad)
+        if idx_bads:
+            cand_fids.remove_rows(idx_bads)
 
-    def get_spoiler_stars(self, fid):
-        """Get stars within FID.spoiler_margin (10 pixels) + dither.  Starcheck uses 25" but this
-        seems small: 20" (4 pix) positional err + 4 pixel readout halfw + 2 pixel PSF
-        width of spoiler star.
+    def set_spoilers_score(self, fid):
+        """Get stars within FID.spoiler_margin (10 pixels) + dither.  Starcheck uses
+        25" but this seems small: 20" (4 pix) positional err + 4 pixel readout
+        halfw + 2 pixel PSF width of spoiler star.
 
         This sets the 'spoilers' column value to a table of spoilers stars (usually empty).
 
         If also sets the 'spoiler_score' to 1 if there is a yellow spoiler
         (4 <= star_mag - fid_mag < 5) or 4 for red spoiler (star_mag - fid_mag < 4).
         The spoiler score is used later to choose an acceptable set of fids and acq stars.
+
         """
         stars = self.meta['stars'][ACQ.spoiler_star_cols]
         dither_pix = self.meta['dither'] / 5
@@ -228,6 +322,10 @@ class FidTable(ACACatalogTable):
                 fid['spoiler_score'] = 4
             elif np.any(yellow):
                 fid['spoiler_score'] = 1
+
+            if fid['spoiler_score'] != 0:
+                self.log(f'Set fid {fid["id"]} spoiler score to {fid["spoiler_score"]}',
+                         level=1)
 
 
 def get_fid_positions(detector, focus_offset, sim_offset):
--- proseco/tests/test_fid.py
+++ proseco/tests/test_fid.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from ..fid import get_fid_positions
+from ..fid import get_fid_positions, get_fid_catalog
+from ..acq import AcqTable
+from chandra_aca.transform import yagzag_to_pixels
 
 
 def test_get_fid_position():
@@ -33,3 +35,55 @@ def test_get_fid_position():
     fidset = [0, 1, 2]
     assert np.allclose(yang[fidset], [-1174, 1224, -1177], rtol=0, atol=1.1)
     assert np.allclose(zang[fidset], [-468, -460, 561], rtol=0, atol=1.1)
+
+
+def test_get_initial_catalog():
+    # Basic catalog with no stars in field.  Standard 2-4-5 config.
+    fids = get_fid_catalog(detector='ACIS-S')
+    exp = ['<FidTable length=6>',
+           '  id    yang     zang     row     col     mag   spoiler_score slot',
+           'int64 float64  float64  float64 float64 float64     int64     str3',
+           '----- -------- -------- ------- ------- ------- ------------- ----',
+           '    1   922.59 -1737.89 -180.05 -344.10    7.00             0  ...',
+           '    2  -773.20 -1742.03  160.79 -345.35    7.00             0    0',
+           '    3    40.01 -1871.10   -2.67 -371.00    7.00             0  ...',
+           '    4  2140.23   166.63 -424.51   39.13    7.00             0    1',
+           '    5 -1826.28   160.17  372.97   36.47    7.00             0    2',
+           '    6   388.59   803.75  -71.49  166.10    7.00             0  ...']
+    assert repr(fids.meta['cand_fids']).splitlines() == exp
+
+    # Make catalogs with some fake stars (at exactly fid positions) that spoil
+    # the fids.
+    stars = fids.meta['cand_fids'].copy()
+    stars['mag_err'] = 0.1
+
+    # Spoil fids 1, 2
+    fids2 = get_fid_catalog(detector='ACIS-S', stars=stars[:2], dither=8)
+    assert np.all(fids2['id'] == [3, 4, 5])
+
+    # Spoil fids 1, 2, 3
+    fids3 = get_fid_catalog(detector='ACIS-S', stars=stars[:3], dither=8)
+    assert np.all(fids3['id'] == [4, 5, 6])
+
+    # Spoil fids 1, 2, 3, 4 => no initial catalog gets found
+    fids4 = get_fid_catalog(detector='ACIS-S', stars=stars[:4], dither=8)
+    assert len(fids4) == 0
+
+    acqs = AcqTable.get_acq_catalog(19387)
+    for acq, fid in zip(acqs[:2], fids[:2]):
+        acq['yang'] = fid['yang'] + 90.0
+        acq['zang'] = fid['zang'] + 90.0
+        acq['row'], acq['col'] = yagzag_to_pixels(acq['yang'], acq['zang'])
+
+    fids5 = get_fid_catalog(detector='ACIS-S', acqs=acqs)
+    exp = ['<FidTable length=6>',
+           '  id    yang     zang     row     col     mag   spoiler_score slot',
+           'int64 float64  float64  float64 float64 float64     int64     str3',
+           '----- -------- -------- ------- ------- ------- ------------- ----',
+           '    1   922.59 -1737.89 -180.05 -344.10    7.00             0    0',
+           '    2  -773.20 -1742.03  160.79 -345.35    7.00             0  ...',
+           '    3    40.01 -1871.10   -2.67 -371.00    7.00             0  ...',
+           '    4  2140.23   166.63 -424.51   39.13    7.00             0  ...',
+           '    5 -1826.28   160.17  372.97   36.47    7.00             0    1',
+           '    6   388.59   803.75  -71.49  166.10    7.00             0    2']
+    assert repr(fids5.meta['cand_fids']).splitlines() == exp
