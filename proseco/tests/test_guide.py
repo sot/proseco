40910e95e8aabef430566d94920ddb65f9cfb3c9
Tom Aldcroft <taldcroft@gmail.com>
2018-09-08 11:01:50 -0400

    Formatting cleanup in test_guide.py

--- proseco/tests/test_guide.py
+++ proseco/tests/test_guide.py
@@ -9,7 +9,6 @@ import pytest
 import mica.starcheck
 from chandra_aca.aca_image import ACAImage, AcaPsfLibrary
 from chandra_aca.transform import mag_to_count_rate, count_rate_to_mag
-import agasc
 
 from ..guide import (get_guide_catalog, check_spoil_contrib, get_pixmag_for_offset,
                      check_mag_spoilers)
@@ -21,24 +20,31 @@ HAS_SC_ARCHIVE = Path(mica.starcheck.starcheck.FILES['data_root']).exists()
 
 
 def test_select():
-    # "random" ra/dec/roll
-    selected = get_guide_catalog(att=(10, 20, 3), date='2018:001', dither=(8, 8), t_ccd=-13, n_guide=5)
+    """
+    Arbitrary ra/dec/roll
+    """
+    selected = get_guide_catalog(att=(10, 20, 3), date='2018:001', dither=(8, 8),
+                                 t_ccd=-13, n_guide=5)
     expected_star_ids = [156384720, 155980240, 156376184, 156381600, 156379416]
     assert selected['id'].tolist() == expected_star_ids
 
 
 @pytest.mark.skipif('not HAS_SC_ARCHIVE', reason='Test requires starcheck archive')
 def test_obsid_19461():
-    # overall poor star field
+    """
+    Overall poor star field
+    """
     selected = get_guide_catalog(obsid=19461, n_guide=5)
     expected_star_ids = [450103048, 450101704, 394003312, 450109160, 450109016]
     assert selected['id'].tolist() == expected_star_ids
 
 
 def test_common_column_obsid_19904():
-    # Should not select 1091705224 which has a column spoiler
-    # Limit the star field to just a handful of stars including the star
-    # and the column spoiler
+    """
+    Should not select 1091705224 which has a column spoiler
+    Limit the star field to just a handful of stars including the star
+    and the column spoiler
+    """
     att = (248.515786, -47.373203, 238.665124)
     agasc_ids = [1091709256, 1091698696, 1091705224, 1091702440, 1091704824]
     date = '2018:001'
@@ -94,15 +100,19 @@ def test_region_contrib():
 
 @pytest.mark.skipif('not HAS_SC_ARCHIVE', reason='Test requires starcheck archive')
 def test_exclude_bad_star():
-    # obsid 17896 attitude
-    # Will need to find another bad star, as this one is now excluded via VAR
+    """
+    Obsid 17896 attitude
+    Will need to find another bad star, as this one is now excluded via VAR
+    """
     selected = get_guide_catalog(obsid=6820)
     assert 614606480 not in selected['id']
 
 
 def test_avoid_trap():
-    # Set up a scenario where a star is selected fine at one roll, and then
-    # confirm that it is not selected when roll places it on the trap
+    """
+    Set up a scenario where a star is selected fine at one roll, and then
+    confirm that it is not selected when roll places it on the trap
+    """
     agasc_ids = [156384720, 156376184, 156381600, 156379416, 156384304]
     date = '2018:001'
     ra = 9.769
@@ -135,27 +145,34 @@ def test_big_dither():
 
 def test_check_pixmag_offset():
     APL = AcaPsfLibrary()
+
     # Then use one star and test with various pixels
     star = {'row': -.5, 'col': .5, 'MAG_ACA': 9.0, 'id': 1}
+
     # Confirm that when the bad pixel is bright enough to shift the centroid
     # by N arcsecs that the mag/offset code agrees
     rs = range(-4, 4)
     cs = range(-4, 4)
     lims = [0.1, 0.2, .5, 1.0, 5.0]
     pixvals = range(250, 5000, 250)
+
     for r_dist, c_dist, lim, pixval in itertools.product(rs, cs, lims, pixvals):
         # Get a new star image every time because centroid_fm messes with it in-place
         star_img = APL.get_psf_image(star['row'], star['col'],
                                      norm=mag_to_count_rate(star['MAG_ACA']))
         pix = ACAImage(np.zeros((8, 8)), row0=-4, col0=-4)
+
         # Get the the non-spoiled centroid
         cr1, cc1, n = star_img.centroid_fm()
         pix.aca[r_dist, c_dist] = pixval
         star_img = star_img + pix.aca
+
         # Spoil with a pixel and get tne new centroid
         cr2, cc2, n = star_img.centroid_fm()
+
         # Get the offset in pixel space
         dr = np.sqrt(((cr1 - cr2) ** 2) + ((cc1 - cc2) ** 2))
+
         # Check that if it is spoiled, it would have been spoiled with the tool
         pmag = get_pixmag_for_offset(star['MAG_ACA'], lim)
         if dr > lim:
@@ -163,10 +180,12 @@ def test_check_pixmag_offset():
 
 
 def test_check_spoil_contrib():
-    # Construct a case where a star spoils the edge of the 8x8
-    # Note that for these mock stars, since we we are checking the status of
-    # the first star, ASPQ1 needs to be nonzero on that star or the
-    # check_spoil_contrib code will bail out before actually doing the check
+    """
+    Construct a case where a star spoils the edge of the 8x8
+    Note that for these mock stars, since we we are checking the status of
+    the first star, ASPQ1 needs to be nonzero on that star or the
+    check_spoil_contrib code will bail out before actually doing the check
+    """
     stars = StarsTable.empty()
     stars.add_fake_star(row=0, col=0, mag=8.0, id=1, ASPQ1=1)
     stars.add_fake_star(row=0, col=-5, mag=6.0, id=2, ASPQ1=0)
@@ -183,15 +202,19 @@ def test_check_spoil_contrib():
 
 
 def test_check_mag_spoilers():
-    # Check that stars that should fail the mag/line test actually fail
+    """
+    Check that stars that should fail the mag/line test actually fail
+    """
     intercept = mag_spoiler['Intercept']
     spoilslope = mag_spoiler['Slope']
     star1 = {'row': 0, 'col': 0, 'MAG_ACA': 9.0, 'mag_err': 0, 'id': 1}
+
     # The mag spoiler check only works on stars that are within 10 pixels in row
     # or column, so don't bother simulating stars outside that distance
     r_dists = np.arange(-9.25, 10, 1)
     c_dists = np.arange(-9.5, 10, 1)
     magdiffs = np.arange(2, -5, -.5)
+
     for r_dist, c_dist, magdiff in itertools.product(r_dists, c_dists, magdiffs):
         star2 = {'row': r_dist, 'col': c_dist, 'MAG_ACA': star1['MAG_ACA'] - magdiff,
                  'mag_err': 0, 'id': 2}
