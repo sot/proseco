9f2785ba74fc49efced53aa412239602d704c934
Javier Gonzalez <javierggt@yahoo.com>
2019-12-17 12:57:36 -0500

    Added changes to use setuptools_scm

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-from proseco import __version__
-
 from setuptools import setup
 
 try:
@@ -11,7 +9,8 @@ setup(name='proseco',
       author='Tom Aldcroft',
       description='Probabilistic star evaluation and catalog optimization',
       author_email='taldcroft@cfa.harvard.edu',
-      version=__version__,
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       zip_safe=False,
       packages=['proseco', 'proseco.tests'],
       package_data={'proseco': ['index_template*.html']},
