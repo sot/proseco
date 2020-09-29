sot/xija

using ska-theme

docs/conf.py
@@ -105,7 +105,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'bootstrap-ska'
+html_theme_options = {
+logotext1: 'Ska!' ,
+logotext2: 'xija',
+logotext3: '',
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

sot/proseco

docs/conf.py
@@ -88,7 +88,12 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'bootstrap-ska'
+html_theme_options = {
+logotext1: 'Ska!' ,
+logotext2: 'proseco',
+logotext3: '',
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the