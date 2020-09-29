sot/xija

using ska-theme (fixup)

docs/conf.py
@@ -107,9 +107,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'bootstrap-ska'
 html_theme_options = {
-logotext1: 'Ska!' ,
-logotext2: 'xija',
-logotext3: '',
+    'logotext1': 'Ska!' ,
+    'logotext2': 'xija',
+    'logotext3': '',
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme

sot/proseco

docs/conf.py
@@ -90,9 +90,9 @@ todo_include_todos = False
 #
 html_theme = 'bootstrap-ska'
 html_theme_options = {
-logotext1: 'Ska!' ,
-logotext2: 'proseco',
-logotext3: '',
+'logotext1': 'Ska!' ,
+'logotext2': 'proseco',
+'logotext3': '',
 }
 
 # Theme options are theme-specific and customize the look and feel of a theme