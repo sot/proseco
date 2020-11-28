536ab3c48a84c4c3e31cf1782b6ca36b6e1e0ec6
Tom Aldcroft <taldcroft@gmail.com>
2020-11-28 06:20:14 -0500

    Better docs and cleanup

--- proseco/diff.py
+++ proseco/diff.py
@@ -10,7 +10,31 @@ from astropy.table import vstack
 """
 
 
-def get_lines(cat, names=None, label=None):
+def get_lines(cat, names=None, label=None, section_lines=True):
+    """Get list of lines representing catalog suitable for diffing.
+
+    This function sorts the catalog into fids, guides, monitors,and acquisition
+    stars and outputs the table into a separate list of lines. In addition:
+
+    - Translate HRC fid ids 7-10 and 11-14 to 1-4. Values in the range 7-14
+      come from starcheck, but proseco uses 1-4.
+    - Change the MON halfwidth to 25. Starcheck uses 20 to match what effective
+      happens on the ACA, but in reality this parameter is fictional.
+    - For guides, set dim=1, res=1, and halfw=25. The guide search parameters
+      are hardwired in the OBC so the catalog values do not matter.
+    - Optionally add a banner at the top with a label identifying the catalog,
+      e.g. with the obsid. This banner is useful for diffing all the catalogs
+      from a load, and helps to ensure that the diff algorithm stays on track.
+    - Guide stars that are BOT are labeled as GU*, and conversely acq stars
+      that are BOT are labeled as AC*. This gives some visibility into BOT stars
+      while keeping the diffs clean.
+
+    :param cat: Table, star catalog
+    :param names: str, list of column names in the output lines for diffing
+    :param label: str, label for catalog used in banner at top of lines
+    :param section_lines: bool, add separator lines between types (default=True)
+    :returns: list
+    """
     if names is None:
         names = 'idx id slot type sz dim res halfw'
     if isinstance(names, str):
@@ -37,15 +61,13 @@ def get_lines(cat, names=None, label=None):
     guides['halfw'] = 25
     guides['type'][guides['type'] == 'BOT'] = 'GU*'
 
-    # Make MON catalog and convert slot in dim (designated track star) to an ID
-    # so comparison is independent of slot differences.
+    # Make MON catalog and convert slot in dim (designated track star).
     ok = np.in1d(cat['type'], ['MON'])
     mons = cat[ok]
-    mons['halfw'] = 20
+    mons['halfw'] = 25
 
     ok = np.in1d(cat['type'], ['ACQ', 'BOT'])
     acqs = cat[ok]
-    acqs['type'] = acqs['type'].astype('U4')
     acqs.sort('id')
     acqs['type'][acqs['type'] == 'BOT'] = 'AC*'
 
@@ -55,15 +77,20 @@ def get_lines(cat, names=None, label=None):
     # Text representation of table with separator lines between GUI, MON, and
     # ACQ sections.
     table_lines = out.pformat_all()
-    # Look for diffs in the first two characters of FID, GUI, GU*, MON, ACQ, AC*
-    types = np.array(out['type'], dtype='U2')
-    idxs = np.flatnonzero(types[:-1] != types[1:])
-    for idx in reversed(idxs):
-        table_lines.insert(idx + 3, table_lines[1])
 
+    # Optionally divide the fid, guide, mon, and acq sections
+    if section_lines:
+        # Look for diffs in the first two characters of FID, GUI, GU*, MON, ACQ, AC*
+        types = np.array(out['type'], dtype='U2')
+        idxs = np.flatnonzero(types[:-1] != types[1:])
+        for idx in reversed(idxs):
+            table_lines.insert(idx + 3, table_lines[1])
+
+    # Finally make a banner and add the table lines
     lines = []
     if label is not None:
-        sep = '=' * max(40, len(label))
+        table_width = max(len(line) for line in table_lines)
+        sep = '=' * max(table_width, len(label))
         lines.append(sep)
         lines.append(label)
         lines.append(sep)
@@ -82,9 +109,9 @@ class DiffHTML:
         return self.html
 
 
-def diff_html(cats1, cats2, out_file=None, labels=None, names=None):
+def diff_html(cats1, cats2, out_file=None, names=None, labels=None, section_lines=True):
     """
-    Calculate the diff of ACA catalogs ``cats1`` and ``cats2`` as HTML.
+    Return the diff of ACA catalogs ``cats1`` and ``cats2`` as HTML.
 
     If ``out_file`` is supplied the output is written to that file name, otherwise
     the output is returned in a ``DiffHTML`` object that will display the
@@ -95,6 +122,8 @@ def diff_html(cats1, cats2, out_file=None, labels=None, names=None):
     :param out_file: str, output file name or Path
     :param names: list of column names (either as list or space-delimited str).
         Default = 'idx id slot type sz dim res halfw'
+    :param label: str, None, label for catalog used in banner at top of lines
+    :param section_lines: bool, add separator lines between types (default=True)
     :returns: None or DiffHTML object
     """
     if not isinstance(cats1, list):
