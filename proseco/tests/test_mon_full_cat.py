200660808557af41a95352711085eb8503399ae5
Tom Aldcroft <taldcroft@gmail.com>
2021-03-12 09:49:59 -0500

    Initial improvements of tests

--- proseco/tests/test_mon_full_cat.py
+++ proseco/tests/test_mon_full_cat.py
@@ -2,17 +2,19 @@
 import os
 
 from pathlib import Path
+from chandra_aca.transform import radec_to_yagzag, yagzag_to_pixels
 
 import numpy as np
+import pytest
 import mica.starcheck
 import agasc
+from astropy.table import Table
 
-from .test_common import mod_std_info, DARK40
-from ..core import StarsTable, includes_for_obsid
-from ..catalog import get_aca_catalog, ACATable
-from ..fid import get_fid_catalog, FidTable
-from .. import characteristics as ACA
+from .test_common import mod_std_info
+from ..core import StarsTable
+from ..catalog import get_aca_catalog
 from ..characteristics import MonCoord, MonFunc
+from .. import characteristics as ACA
 
 HAS_SC_ARCHIVE = Path(mica.starcheck.starcheck.FILES['data_root']).exists()
 TEST_COLS = 'slot idx id type sz yang zang dim res halfw'.split()
@@ -21,16 +23,46 @@ TEST_COLS = 'slot idx id type sz yang zang dim res halfw'.split()
 os.environ[agasc.SUPPLEMENT_ENABLED_ENV] = 'False'
 
 
-def test_monitor_input_processing():
-    stars = StarsTable.empty()
-    stars.add_fake_constellation(mag=10.5 - np.arange(8) * 0.5, id=np.arange(100000, 100008))
+@pytest.fixture
+def stars():
+    out = StarsTable.empty()
+    out.add_fake_constellation(mag=10.5 - np.arange(8) * 0.5, id=np.arange(100000, 100008))
+    return out
+
+
+def test_monitor_input_processing_ra_dec(stars):
+    ra, dec = 0.1, 0.2
+    mag = 7.0
+    monitors = [[ra, dec, MonCoord.RADEC, mag, MonFunc.MON_FIXED]]
+    aca = get_aca_catalog(**mod_std_info(att=stars.att, n_fid=0, n_guide=5),
+                          stars=stars, monitors=monitors)
+    mons = aca.guides.monitors
+    assert len(mons) == 1
+    assert isinstance(mons, Table)
+    yang, zang = radec_to_yagzag(ra, dec, aca.att)
+    row, col = yagzag_to_pixels(yang, zang)
+    assert np.allclose(mons['yang'][0], yang)
+    assert np.allclose(mons['zang'][0], zang)
+    assert np.allclose(mons['ra'][0], ra)
+    assert np.allclose(mons['dec'][0], dec)
+    assert np.allclose(mons['row'][0], row)
+    assert np.allclose(mons['col'][0], col)
+
+    ok = aca['type'] == 'MON'
+    assert np.count_nonzero(ok) == 1
+    mon = aca[ok][0]
+    assert np.allclose(mon['yang'], yang)
+    assert np.allclose(mon['zang'], zang)
+    assert np.allclose(mon['mag'], mag)
+    assert np.allclose(mon['maxmag'], ACA.monitor_maxmag)
+    assert mon['id'] == 1000
 
 
 def test_monitor_mon_fixed_auto():
     """In this case the MON_TRACK slot is not near a star"""
-    monitors = [[-1700, 1900, ACA.MonCoord.YAGZAG, 7.5, ACA.MonFunc.MON_FIXED],
-                [50, -50, ACA.MonCoord.ROWCOL, 7.5, ACA.MonFunc.MON_TRACK],
-                [1053.38, -275.16, ACA.MonCoord.YAGZAG, 8.0, ACA.MonFunc.AUTO]
+    monitors = [[-1700, 1900, MonCoord.YAGZAG, 7.5, MonFunc.MON_FIXED],
+                [50, -50, MonCoord.ROWCOL, 7.5, MonFunc.MON_TRACK],
+                [1053.38, -275.16, MonCoord.YAGZAG, 8.0, MonFunc.AUTO]
                 ]
     aca = get_aca_catalog(monitors=monitors, **mod_std_info(n_guide=6, n_fid=2),
                           include_ids_guide=[611192064])
@@ -74,13 +106,12 @@ def test_full_catalog():
     stars.add_fake_star(yang=400, zang=400, mag=10.5, id=100008)
     stars.add_fake_star(yang=100, zang=200, mag=6.5, id=50)
     stars.add_fake_star(yang=-1000, zang=-2000, mag=6.25, id=51)
-    monitors = [[100, 200, ACA.MonCoord.YAGZAG, 6.123, ACA.MonFunc.GUIDE],
-                [-1000, -2000, ACA.MonCoord.YAGZAG, 6.123, ACA.MonFunc.GUIDE],
-                [0, 0, ACA.MonCoord.YAGZAG, 6.123, ACA.MonFunc.AUTO]]
+    monitors = [[100, 200, MonCoord.YAGZAG, 6.123, MonFunc.GUIDE],
+                [-1000, -2000, MonCoord.YAGZAG, 6.123, MonFunc.GUIDE],
+                [0, 0, MonCoord.YAGZAG, 6.123, MonFunc.AUTO]]
 
     aca = get_aca_catalog(**mod_std_info(att=stars.att, n_fid=1, n_guide=7), stars=stars,
-                          monitors=monitors, exclude_ids_acq=[100000, 100001, 51, 100003],
-                          dark=DARK40)
+                          monitors=monitors, exclude_ids_acq=[100000, 100001, 51, 100003])
 
     exp = ['slot idx   id   type  sz   yang     zang   dim res halfw  mag ',
            '---- --- ------ ---- --- -------- -------- --- --- ----- -----',
@@ -102,7 +133,7 @@ def test_full_catalog():
 def test_mon_takes_guide():
     stars = StarsTable.empty()
     stars.add_fake_constellation(mag=6.5 + np.arange(8) * 0.5, id=np.arange(100000, 100008))
-    monitors = [[1500, 0, ACA.MonCoord.YAGZAG, 6.123, ACA.MonFunc.MON_TRACK]]
+    monitors = [[1500, 0, MonCoord.YAGZAG, 6.123, MonFunc.MON_TRACK]]
 
     aca = get_aca_catalog(**mod_std_info(att=stars.att, n_fid=0, n_guide=5, n_acq=5), stars=stars,
                           monitors=monitors)
