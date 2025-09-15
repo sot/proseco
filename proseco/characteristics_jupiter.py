a566cc8119d93767898fd5c40e2dc420435df84b
Jean Connelly <jconnelly@cfa.harvard.edu>
2025-09-15 09:08:19 -0400

    Add comment with more human-readable dates

--- proseco/characteristics_jupiter.py
+++ proseco/characteristics_jupiter.py
@@ -1,4 +1,5 @@
-# date range when jupiter vmag >= -2.0 (dim)
+# Date range when jupiter vmag >= -2.0 (dim) and doesn't need to be checked.
+# This cycle this is 2026-05-04 to 2026-11-02.
 exclude_dates = [
     {
         "start": "2026:124",
